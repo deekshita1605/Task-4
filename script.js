9eee2c44c136e97c4a5d8961983b73bd37ecea56
deekshita1605 <deekshita16102005@gmail.com>
2026-06-21 15:47:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function displayTasks() {
+    let taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            ${task}
+            <button onclick="deleteTask(${index})">❌</button>
+        `;
+
+        taskList.appendChild(li);
+    });
+}
+
+function addTask() {
+    let input = document.getElementById("taskInput");
+
+    if (input.value.trim() === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    tasks.push(input.value);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    input.value = "";
+
+    displayTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    displayTasks();
+}
+
+displayTasks();
